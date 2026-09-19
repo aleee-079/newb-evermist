6c45da439b79d8b98f8960f2e0efc82e68a1531f
aleee-079 <jesusperez153077@gmail.com>
2026-09-19 14:45:08 -0400

    Add files via upload

--- src/newb/config.h
+++ src/newb/config.h
@@ -71,11 +71,11 @@
 #define NL_SKY_RAIN_MIX_FACTOR 0.9
 
 /* Sky colors - zenith=top, horizon=bottom */
-#define NL_DAWN_ZENITH_COL   vec3(0.2,0.7,2.0)
+#define NL_DAWN_ZENITH_COL   vec3(0.1,0.5,1.5)
 #define NL_DAWN_HORIZON_COL  vec3(1.65,0.20,0.05)  * 1.630
 #define NL_DAWN_EDGE_COL     vec3(2.0,0.9,0.9)
 #define NL_DAY_ZENITH_COL    vec3(0.3,0.9,2.0)
-#define NL_DAY_HORIZON_COL   vec3(0.3,0.9,2.0)
+#define NL_DAY_HORIZON_COL   vec3(0.0,0.7,2.3)
 #define NL_DAY_EDGE_COL      vec3(1.44,1.56,1.62)
 #define NL_NIGHT_ZENITH_COL  vec3(0.02,0.05,0.2)
 #define NL_NIGHT_HORIZON_COL vec3(0.05,0.0,0.3)
@@ -106,19 +106,19 @@
 
 /* Water */
 #define NL_WATER_TRANSPARENCY 0.8 // 0.0 transparent ~ 1.0 normal
-#define NL_WATER_BUMP 0.0        // 0.001 plain ~ 0.2 bumpy water
-#define NL_WATER_WAVE_SPEED  0.0  // 0.2 calm ~ 2.0 turbulent
+#define NL_WATER_BUMP 0.05        // 0.001 plain ~ 0.2 bumpy water
+#define NL_WATER_WAVE_SPEED  0.2  // 0.2 calm ~ 2.0 turbulent
 #define NL_WATER_TEX_OPACITY 0.0  // 0.0 plain water ~ 1.0 vanilla water texture
 #define NL_WATER_WAVE             // [toggle] wave effect
 //#define NL_WATER_REFL_MASK      // [toggle] fake water reflection mask
 #define NL_WATER_TINT vec3(0.0,0.0,0.10)
 
 /* Underwater */
-#define NL_UNDERWATER_BRIGHTNESS 1.65         // 0.0 dark ~ 3.0 bright
-#define NL_CAUSTIC_INTENSITY 2.35             // 0.5 weak ~ 5.0 bright
-#define NL_UNDERWATER_WAVE 0.06               // [toggle] 0.02 subtle ~ 0.6 trippy
-#define NL_UNDERWATER_STREAKS 1.35            // [toggle] 0.8 subtle - 2.0 bright streaks from top
-#define NL_UNDERWATER_TINT vec3(0.,0.18,0.18) // fog tint color when underwater
+#define NL_UNDERWATER_BRIGHTNESS 4.0         // 0.0 dark ~ 3.0 bright
+#define NL_CAUSTIC_INTENSITY 4.0             // 0.5 weak ~ 5.0 bright
+#define NL_UNDERWATER_WAVE 0.02               // [toggle] 0.02 subtle ~ 0.6 trippy
+#define NL_UNDERWATER_STREAKS 1.5            // [toggle] 0.8 subtle - 2.0 bright streaks from top
+#define NL_UNDERWATER_TINT vec3(0.0,0.0,0.10) // fog tint color when underwater
 
 /* Cloud type */
 #define NL_CLOUD_TYPE 2 // 0:vanilla, 1:soft, 2:rounded, 3:realistic
